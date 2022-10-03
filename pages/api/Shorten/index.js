0125d36df51d3e2e1ac1628ece77720fe38eb3b5
Midhunkrishna k s <50834905+smidhunkks@users.noreply.github.com>
2022-10-03 21:57:55 +0530

    connect database

--- pages/api/Shorten/index.js
+++ pages/api/Shorten/index.js
@@ -1,4 +1,12 @@
-function handler(req, res) {
+import connection from "../db";
+
+  function handler (req, res) {
+  connection.query('select * from urls;',(err,result)=>{
+    console.log(result);
+  });
+ 
+
+  // connection.query('insert into urls (url) values ("hello world")')
   res.status(200).json({ name: "Home" });
 }
 
---
+++ pages/api/db.js
@@ -0,0 +1,6 @@
+require('dotenv').config()
+const mysql = require('mysql2')
+const connection = mysql.createConnection(process.env.DATABASE_URL)
+console.log('Connected to PlanetScale!')
+// connection.end()
+ module.exports=connection;
